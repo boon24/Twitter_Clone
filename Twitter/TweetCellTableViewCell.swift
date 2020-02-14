0417ce51239cac2c41c53a6d4d3796bfeaa1e3ac
Haroon Zia <haroonzia24@gmail.com>
2020-02-13 23:33:57 -0800

    fixed retweet button

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,86 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Kay Lab on 2/13/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    var favorited: Bool = false
+    var tweetId: Int = -1
+    //var retweeted: Bool = false
+    
+    func setFavorited(_ isFavorited: Bool) {
+        favorited = isFavorited
+        if favorited {
+            favButton.setImage(UIImage(named: "favor-icon-red"), for: UIControl.State.normal)
+        } else {
+            favButton.setImage(UIImage(named: "favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+    let tobeFavorited = !favorited
+    if(tobeFavorited){
+        TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+            self.setFavorited(true)
+        }, failure: { (error) in
+            print("favorited not succed \(error)")
+        })
+    }else{
+        TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+            self.setFavorited(false)
+        }, failure: { (error) in
+            print("unfavorited not succed \(error)")
+        })
+        
+    }
+    }
+    
+    @IBAction func retweet(_ sender: Any){
+    TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+        self.setRetweeted(true)
+    }, failure: { (error) in
+        print("unable retweet \(error)")
+    })
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setRetweeted(_ isRetweeted: Bool) {
+        if isRetweeted {
+            retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            retweetButton.isEnabled = false
+        } else {
+            retweetButton.setImage(UIImage(named: "retweet-icon"), for: UIControl.State.normal)
+            retweetButton.isEnabled = true
+        }
+        
+        }
+
+}
